7c4156618e4ff7d483ad568b835c7d154a960a6d
Eran Leasure <eran.leasure@gmail.com>
2017-03-16 15:04:05 -0400

    initial commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Leasure, Eran on 3/16/17.
+//  Copyright © 2017 Bedrock Detroit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        // increment tapCount by 1
+        tapCount += 1
+        
+        if tapCount == 10 {
+            theLabel.text = "You tapped the button 10 times!"
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
